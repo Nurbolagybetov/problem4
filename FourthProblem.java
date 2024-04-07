5bcd0dbd2752342b39669688b7dbb35d9a03982a
Nurbolagybetov <154108956+Nurbolagybetov@users.noreply.github.com>
2024-04-07 22:30:52 +0500

    Add files via upload

---
+++ FourthProblem.java
@@ -0,0 +1,29 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class FourthProblem {
+    /*
+     *This method returns factorial of the number that we inputed.
+     *Time complexity: 0(n), where n is number that we input.
+     *Complexity class: Linear
+     *@param n The is number that we input.
+     *@return factorial by using recursion
+     */
+    public static void main(String[] arg){
+        double startTime = System.nanoTime();
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Time taken " + duration + " milliseconds");
+        int f = factorial(n);
+        System.out.println(f);
+    }
+    public static int factorial(int n){
+        if(n<=1){
+            return 1;
+        }
+        return factorial(n - 1) * n;
+    }
+}
