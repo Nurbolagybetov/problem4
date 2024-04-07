9c824322053a3ff7334ae70aa0212efee083b7dd
Nurbolagybetov <154108956+Nurbolagybetov@users.noreply.github.com>
2024-04-07 22:31:56 +0500

    Update FourthProblem.java

--- FourthProblem.java
+++ FourthProblem.java
@@ -3,13 +3,6 @@ package assignment1;
 import java.util.Scanner;
 
 public class FourthProblem {
-    /*
-     *This method returns factorial of the number that we inputed.
-     *Time complexity: 0(n), where n is number that we input.
-     *Complexity class: Linear
-     *@param n The is number that we input.
-     *@return factorial by using recursion
-     */
     public static void main(String[] arg){
         double startTime = System.nanoTime();
         double endTime = System.nanoTime();
@@ -20,6 +13,13 @@ public class FourthProblem {
         int f = factorial(n);
         System.out.println(f);
     }
+    /*
+     *This method returns factorial of the number that we inputed.
+     *Time complexity: 0(n), where n is number that we input.
+     *Complexity class: Linear
+     *@param n The is number that we input.
+     *@return factorial by using recursion
+     */
     public static int factorial(int n){
         if(n<=1){
             return 1;
